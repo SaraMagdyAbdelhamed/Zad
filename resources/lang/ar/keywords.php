8ed9bc055370f8d50193dfdf372d709e3117c93f
SaraMagdyAbdelhamed <sara.magdy@pentavalue.com>
2018-07-15 17:22:26 +0200

    add_keywords

---
+++ resources/lang/ar/keywords.php
@@ -0,0 +1,22 @@
+<?php
+ 
+    // keywords array
+    return [
+        // general names
+        'website_name'      => 'ايڤينتكوم',
+        'login_page_name'   => 'صفحة الدخول',
+        'login_username'    => 'اسم المستخدم',
+        'login_password'    => 'كلمة السر',
+        'logout'            => 'الخروج',
+        'language'          => 'العربية',
+        'email'             => 'البريد الالكتروني',
+        'not'               => 'لا يوجد',
+        'addNew'            => 'اضافة'
+        
+
+
+	
+	];
+
+
+?>
---
+++ resources/lang/en/keywords.php
@@ -0,0 +1,18 @@
+<?php
+
+    // keywords array
+    return [
+        // general names
+        'website_name'      => 'Eventakom',
+        'login_page_name'   => 'Login',
+        'login_username'    => 'username',
+        'login_password'    => 'password',
+        'logout'            => 'Logout',
+        'language'          => 'English',
+        'email'             => 'Email',
+        'not'               => '',
+        'addNew'            => 'Add new'
+		
+    ];
+
+?>
